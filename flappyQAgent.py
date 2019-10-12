8dc1afd16f5fcfeab764c3ef99bf2157208029c3
hakosaj <jussi.hakosalo@aalto.fi>
2019-10-12 19:46:08 +0000

    Bugfixing, additions to the Q-learning algorithm.

---
+++ flappyQAgent.py
@@ -0,0 +1,19 @@
+import sys
+import numpy
+import os
+import random
+import time
+import pygame
+import math
+from flappyConstants import *
+from chromosome import Chromosome
+from bird import Bird
+from selection import *
+from pillar import Pillar
+from pygame.locals i
+
+    #ata = np.zeros((580,600,2))
+#y: [-280,300]
+#x: [600,0]
+
+class FlappyQAgent: 
